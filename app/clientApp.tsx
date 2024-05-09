ddfa98eb01b5fb1a9f9c34361d14a2aebf825c1c
Moe Martinez <deadfocus@gmail.com>
2024-05-09 11:55:04 -0500

    dont look at my any type

--- app/clientApp.tsx
+++ app/clientApp.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState, useEffect } from 'react';
 
-export default function ClientApp({ children }) {
+export default function ClientApp({ children } : any) {
   useEffect(() => {
     scrolling();
   });
